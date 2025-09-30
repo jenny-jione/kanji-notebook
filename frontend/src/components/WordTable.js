87b418a2604e7d9b0ccf4b83857b44c95f0ce302
jenny.jione.jang <jenny.jione.jang@gmail.com>
2025-09-30 19:56:54 +0900

    feat: 단어 테이블 구현 - 전체 목록, 특정 한자 페이지 공통으로 사용하는 컴포넌트

---
+++ frontend/src/components/WordTable.js
@@ -0,0 +1,45 @@
+// WordTable.js
+import { Link, useNavigate } from "react-router-dom";
+
+function WordTable({ words }) {
+  const navigate = useNavigate();
+
+  return (
+    <table border="1" cellPadding="5" cellSpacing="0" style={{ borderCollapse: "collapse" }}>
+      <thead>
+        <tr>
+          <th>단어</th>
+          <th>히라가나</th>
+          <th>뜻</th>
+          <th>한국어 발음</th>
+          <th>관련 링크</th>
+        </tr>
+      </thead>
+      <tbody>
+        {words.map((item) => (
+          <tr key={item.word}>
+            <td>{item.word}</td>
+            <td>{item.reading}</td>
+            <td>{item.meaning}</td>
+            <td>{item.korean}</td>
+            <td>
+              <div>
+                {item.kanji_list.map((kanji) => (
+                  <button
+                    key={kanji}
+                    style={{ margin: "5px", fontSize: "15px" }}
+                    onClick={() => navigate(`/kanji/${kanji}`)}
+                  >
+                    {kanji}
+                  </button>
+                ))}
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default WordTable;
