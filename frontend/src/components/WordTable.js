4a04f3a4e456c21126ec1e058eb89c0ab05ca26a
jenny.jione.jang <jenny.jione.jang@gmail.com>
2025-10-01 15:43:49 +0900

    fix: remove unused import

--- frontend/src/components/WordTable.js
+++ frontend/src/components/WordTable.js
@@ -1,5 +1,5 @@
 // WordTable.js
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function WordTable({ words }) {
   const navigate = useNavigate();
